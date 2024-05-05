a8614b66c93da667d4d9ed8d77564bfc33cd8708
Hemant2335 <knrt73373@gmail.com>
2024-05-05 11:44:12 +0530

    Minor_change_cors

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,11 @@ import WebSocket, { WebSocketServer } from "ws";
 
 app.use(express.json());
 app.use(cookieParser());
+app.use()
 app.use(cors({
   credentials: true,
-  origin: ["https://brainopfrontend.vercel.app", "http://localhost:3000"]
+  origin: ["https://brainopfrontend.vercel.app", "http://localhost:3000"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 app.get('/', (req, res) => {
