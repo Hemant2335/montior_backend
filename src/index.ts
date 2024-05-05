597f6309995ab5a75a4d36069fd93abcb3223dc2
Hemant2335 <knrt73373@gmail.com>
2024-05-05 11:57:56 +0530

    frontend_route_changed

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: ["https://brainopfrontend.vercel.app", "http://localhost:3000"],
+  origin: ["https://monitordevices.vercel.app", "http://localhost:3000"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
